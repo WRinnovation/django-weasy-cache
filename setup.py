22f0e9d26791cd987a638b528de5822a011cdc92
Matteo Senardi <matteo.senardi@webranking.it>
2017-10-31 11:11:29 +0100

    modifica setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-weasy-cache',
     version='0.1dev',
     packages=['djangoweasycache', ],
     requires=['python (>= 2.7)', 'django (>=1.8)'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='Apache2',
     long_description=open('README.txt').read(),
+    keywords='django simple cache',
 )
