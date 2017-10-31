9276d2c829dfb01efd0b19a5a5e9cb4bac86bd67
Matteo Senardi <matteo.senardi@webranking.it>
2017-10-31 11:05:04 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='django-weasy-cache',
     version='0.1dev',
     packages=['djangoweasycache', ],
+    requires=['python (>= 2.7)', 'django (>=1.8)'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
