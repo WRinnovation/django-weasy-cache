3bf1c1a3e44b8053aae304da5a08bd410fc2c34f
Matteo Senardi <matteo.senardi@webranking.it>
2017-11-07 10:51:32 +0100

    added override_key option to override default cache key

--- djangoweasycache/__init__.py
+++ djangoweasycache/__init__.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def join_(*args):
     rv = ""
     for _string in args:
-        rv += ' ' + _string
+        rv += ' ' + str(_string)
     return rv.lstrip()
 
 
@@ -26,24 +26,25 @@ def cache_get_key(*args, **kwargs):
 
 
 # decorator for caching functions
-def cache_for(cache_label, time=None, default_time=False):
+def cache_for(cache_label, time=None, default_time=False, override_key=None):
     """
-    :param cache_label: key for django cache
-    :param time: timeout in seconds
-    :param default_time: if True uses default django timeout defined in settings
-    :return: result of decorated function
-    """
+        :param cache_label: key for django cache
+        :param time: timeout in seconds
+        :param default_time: if True uses default django timeout defined in settings
+        :param override_key: if not None defines cache key
+        :return: result of decorated function
+        """
     def decorator(fn):
         def wrapper(*args, **kwargs):
             cache = caches[cache_label]
-            key = cache_get_key(fn.__name__, *args, **kwargs)
+            key = cache_get_key(fn.__name__, [override_key], **{}) if override_key is not None else cache_get_key(fn.__name__, *args, **kwargs)
             result = cache.get(key)
             if not result:
                 result = fn(*args, **kwargs)
                 cache.set(key, result, time) if default_time is False else cache.set(key, result)
-                logger.debug('Cache {} set {}'.format(cache_label, join_(*args)))
+                print('Cache {} set {}'.format(cache_label, join_(*args)))
             else:
-                logger.debug('Cache {} hit {}'.format(cache_label, join_(*args)))
+                print('Cache {} hit {}'.format(cache_label, join_(*args)))
             return result
 
         return wrapper
